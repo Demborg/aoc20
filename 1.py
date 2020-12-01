6692f53198efe39fb00c24bb77a5b5634f04cd7b
Axel Demborg <axel.demborg@volumental.com>
2020-12-01 20:05:02 +0100

    read from stdin

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
-data = [int(l) for l in open("1.txt").read().split("\n") if l]
+import sys
+data = [int(l) for l in sys.stdin.read().split("\n") if l]
 
 print([(a * b) for a in data for b in data if a + b == 2020])
 print([(a * b * c) for a in data for b in data for c in data if sum((a, b, c)) == 2020])
