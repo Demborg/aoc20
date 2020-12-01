9685ca4d6762f04a71dddbe370ebcbfb5c834648
Axel Demborg <axel.demborg@volumental.com>
2020-12-01 20:18:49 +0100

    Remove duplication

--- 1.py
+++ 1.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 from math import prod
 from sys import stdin
-data = [int(l) for l in stdin.read().split("\n") if l]
 
-print(next(prod(l) for l in combinations(data, 2) if sum(l) == 2020))
-print(next(prod(l) for l in combinations(data, 3) if sum(l) == 2020))
+data = [int(l) for l in stdin.read().split("\n") if l]
+print([next(prod(l) for l in combinations(data, n) if sum(l) == 2020) for n in (2, 3)])
