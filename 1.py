daa8f1e555e7b4d61947177319f05d0b788dcf9a
Axel Demborg <axel.demborg@volumental.com>
2020-12-01 20:13:20 +0100

    rewrite using itertools

--- 1.py
+++ 1.py
@@ -1,5 +1,7 @@
-import sys
-data = [int(l) for l in sys.stdin.read().split("\n") if l]
+from itertools import combinations
+from math import prod
+from sys import stdin
+data = [int(l) for l in stdin.read().split("\n") if l]
 
-print([(a * b) for a in data for b in data if a + b == 2020])
-print([(a * b * c) for a in data for b in data for c in data if sum((a, b, c)) == 2020])
+print(next(prod(l) for l in combinations(data, 2) if sum(l) == 2020))
+print(next(prod(l) for l in combinations(data, 3) if sum(l) == 2020))
