474197f1a7472a00c208df7aca981e56a484040b
Axel Demborg <axel.demborg@volumental.com>
2020-12-02 07:15:31 +0100

    bit more general parsing for 2.2

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 from sys import stdin
 
 data = [l.split(" ") for l in stdin.read().split("\n") if l]
-parsed = [[l[2].count(l[1][0]), [int(i) for i in l[0].split("-")]] for l in data]
-print(len([p for p in parsed if p[1][0] <= p[0] <= p[1][1]]))
+parsed = [[l[2], l[1][0], [int(i) for i in l[0].split("-")]] for l in data]
+print(len([p for p in parsed if p[2][0] <= p[0].count(p[1]) <= p[2][1]]))
