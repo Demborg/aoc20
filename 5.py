956f96d8cd2123e0888cf76d528c155935d5c9a1
Axel Demborg <axel.demborg@volumental.com>
2020-12-05 21:44:43 +0100

    cleaner output

--- 5.py
+++ 5.py
@@ -2,4 +2,4 @@ from sys import stdin
 
 ids = [sum( 2 ** i * (c in "BR") for i, c in enumerate(l[9::-1])) for l in stdin.readlines()]
 print(max(ids))
-print([v for v in range(min(ids), max(ids)) if v not in ids])
+print(next(v for v in range(min(ids), max(ids)) if v not in ids))
