347ce23ac9a68ddb11579b0ecfc9864573a88460
Axel Demborg <axel.demborg@volumental.com>
2020-12-05 21:41:17 +0100

    dang, you with your rows and cols...

--- 5.py
+++ 5.py
@@ -1,10 +1,5 @@
 from sys import stdin
 
-def decode(data, c):
-    return sum(2 ** i * (d == c) for i, d in enumerate(data))
-
-data = [[decode(l[6::-1], "B"), decode(l[9:6:-1], "R")] for l in stdin.readlines()]
-ids = [d[0] * 8 + d[1] for d in data]
-
+ids = [sum( 2 ** i * (c in "BR") for i, c in enumerate(l[9::-1])) for l in stdin.readlines()]
 print(max(ids))
 print([v for v in range(min(ids), max(ids)) if v not in ids])
