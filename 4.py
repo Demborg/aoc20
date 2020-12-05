34217542967fb8e04496f2982ea57e357e2f34bb
Axel Demborg <axel.demborg@volumental.com>
2020-12-05 20:40:11 +0100

    add rules for fields

--- 4.py
+++ 4.py
@@ -1,6 +1,15 @@
 import re
 from sys import stdin
 
-required = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+required = {
+    "byr": ("(\d{4})", lambda x: 1920 <= int(x) <= 2002),
+    "iyr": ("(\d{4})", lambda x: 2010 <= int(x) <= 2020),
+    "eyr": ("(\d{4})", lambda x: 2020 <= int(x) <= 2030),
+    "hgt": ("(\d*)(in|cm)", lambda x, m: (150 if m == "cm" else 59) <= int(x) <= (193 if m == "cm" else 76)),
+    "hcl": ("(#[\da-f]{6})", lambda x: True),
+    "ecl": ("(amb|blu|brn|gry|grn|hzl|oth)", lambda x: True),
+    "pid": ("(\d{9})", lambda x: True)
+}
 data = [{(t := d.split(":"))[0]: t[1] for d in p.split() if d} for p in stdin.read().replace("\n", " ").split("  ") if p]
 print(sum(1 for d in data if all(r in d for r in required)))
+
