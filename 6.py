6ed2ad649c74a8b6a2173d940ede2219566b25f2
Axel Demborg <axel.demborg@volumental.com>
2020-12-06 08:36:58 +0100

    more general data loading for part 2

--- 6.py
+++ 6.py
@@ -1,4 +1,4 @@
 from sys import stdin
 
-data = [{q for p in g.split("\n") for q in p} for g in stdin.read().split("\n\n")]
-print(sum(len(g) for g in data))
+data = [[{q for q in p} for p in g.split("\n") if p] for g in stdin.read().split("\n\n")]
+print(sum(len({q for p in g for q in p}) for g in data))
