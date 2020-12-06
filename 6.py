f5d9f6f67657db95ef2336afb236b0cf46de2d2e
Axel Demborg <axel.demborg@volumental.com>
2020-12-06 08:57:08 +0100

    clean up

--- 6.py
+++ 6.py
@@ -1,5 +1,5 @@
 from functools import reduce
 from sys import stdin
 
-data = [[{q for q in p} for p in g.split("\n") if p] for g in stdin.read().split("\n\n")]
+data = [[set(p) for p in g.split("\n") if p] for g in stdin.read().split("\n\n")]
 print([sum(len(reduce(f, g)) for g in data) for f in (set.union, set.intersection)])
