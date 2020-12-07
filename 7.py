67c68a639fda16e5b4d7c4403ea6f74e8fa31973
Axel Demborg <axel.demborg@volumental.com>
2020-12-07 20:05:29 +0100

    7.2 solved but in serious need of love

--- 7.py
+++ 7.py
@@ -2,14 +2,25 @@ import re
 from itertools import groupby
 from sys import stdin
 
-data = [[i, re.match("\w* \w*", l).group()] for l in stdin.read().split("\n") for i in re.findall("\d (\w* \w*)", l)]
-grouped = {g: list(d) for g, d in groupby(sorted(data, key=lambda x: x[0]), lambda x: x[0])}
+data = [[*i, re.match("\w* \w*", l).group()] for l in stdin.read().split("\n") for i in re.findall("(\d) (\w* \w*)", l)]
+back = {g: list(d) for g, d in groupby(sorted(data, key=lambda x: x[1]), lambda x: x[1])}
+forward = {g: list(d) for g, d in groupby(sorted(data, key=lambda x: x[2]), lambda x: x[2])}
 
 visited = set()
-que = [i[1] for i in grouped["shiny gold"]]
+que = [i[2] for i in back["shiny gold"]]
 while que:
-    v = que.pop()
-    if v not in visited:
-        visited.add(v)
-        que.extend(i[1] for i in grouped.get(v, []))
+    k = que.pop()
+    if k not in visited:
+        visited.add(k)
+        que.extend(i[2] for i in back.get(k, []))
 print(len(visited))
+
+counter = 0
+que = [(1, "shiny gold")]
+while que:
+    n, k = que.pop()
+    v = forward.get(k, [])
+    for i in v:
+        counter += n * int(i[0])
+        que += [(n * int(i[0]), i[1])]
+print(counter)
