1cc17ea48efc6dfe8e483fc87cd4b81f01819959
Axel Demborg <axel.demborg@volumental.com>
2020-12-07 21:51:32 +0100

    remove cryptic lambda

--- 7.py
+++ 7.py
@@ -7,9 +7,9 @@ b, f = ({g: list(d) for g, d in groupby(sorted(data, key=k), k)} for k in (lambd
 
 def one(key):
     return set.union({key}, *(one(i[2]) for i in b.get(key, [])))
+print(len(one("shiny gold")) - 1)
 
 def two(key, n=1):
     return n + sum(two(i[1], n * int(i[0])) for i in f.get(key, []))
-
-print(*(f("shiny gold") - 1 for f in (lambda k: len(one(k)), two)))
+print(two("shiny gold") - 1)
 
