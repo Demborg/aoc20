0269afd9851a62c676f9ac63f3db67c2d3ac3f81
Axel Demborg <axel.demborg@volumental.com>
2020-12-07 21:12:11 +0100

    Recursion for fun and glory

--- 7.py
+++ 7.py
@@ -3,23 +3,13 @@ from itertools import groupby
 from sys import stdin
 
 data = [[*i, re.match("\w* \w*", l).group()] for l in stdin.read().split("\n") for i in re.findall("(\d) (\w* \w*)", l)]
-back, forward = ({g: list(d) for g, d in groupby(sorted(data, key=k), k)} for k in (lambda x: x[i] for i in (1, 2)))
+b, f = ({g: list(d) for g, d in groupby(sorted(data, key=k), k)} for k in (lambda x: x[i] for i in (1, 2)))
 
-visited = set()
-que = [i[2] for i in back["shiny gold"]]
-while que:
-    k = que.pop()
-    if k not in visited:
-        visited.add(k)
-        que.extend(i[2] for i in back.get(k, []))
-print(len(visited))
+def one(key):
+    return set.union({key}, *(one(i[2]) for i in b.get(key, [])))
 
-counter = 0
-que = [(1, "shiny gold")]
-while que:
-    n, k = que.pop()
-    v = forward.get(k, [])
-    for i in v:
-        counter += n * int(i[0])
-        que += [(n * int(i[0]), i[1])]
-print(counter)
+def two(key, n=1):
+    return n + sum(two(i[1], n * int(i[0])) for i in f.get(key, []))
+
+print(*(f("shiny gold") - 1 for f in (lambda k: len(one(k)), two)))
+
