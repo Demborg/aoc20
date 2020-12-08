b5012406de42f60f6635fcfd3f3181e72db86db9
Axel Demborg <axel.demborg@volumental.com>
2020-12-08 21:20:54 +0100

    bit better formatting

--- 8.py
+++ 8.py
@@ -13,4 +13,7 @@ def exec(program, visited=(), idx=0, acc=0):
     return exec(program, visited, idx, acc)
         
 print(exec(program)[1])
-print(next(r[1] for r in (exec([*program[:idx], ["jmp" if i == "nop" else "nop", v], *program[idx + 1:]]) for idx, (i, v) in enumerate(program) if i != "acc") if r[0]))
+print(next(r[1] for r in (
+    exec([*program[:idx], ["jmp" if i == "nop" else "nop", v], *program[idx + 1:]])
+    for idx, (i, v) in enumerate(program) if i != "acc"
+) if r[0]))
