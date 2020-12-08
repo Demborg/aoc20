8205981f0c331b95b8b42f342c26323bb8a02b4c
Axel Demborg <axel.demborg@volumental.com>
2020-12-08 21:16:27 +0100

    remove argument

--- 8.py
+++ 8.py
@@ -3,14 +3,14 @@ from sys import stdin
 
 program = [[i, int(v)] for i, v in re.findall("(\w{3}) (.\d*)", stdin.read())]
 
-def exec(program, visited, idx=0, acc=0):
+def exec(program, visited=(), idx=0, acc=0):
     if (end := idx >= len(program)) or idx in visited:
         return (end, acc) 
-    visited.add(idx)
+    visited += (idx, )
     i, v = program[idx]
     idx += v if i == "jmp" else 1
     acc += v if i == "acc" else 0
     return exec(program, visited, idx, acc)
         
-print(exec(program, set())[1])
-print(next(r for r in (exec([*program[:idx], ["jmp" if i == "nop" else "nop", v], *program[idx + 1:]], set()) for idx, (i, v) in enumerate(program) if i != "acc") if r[0]))
+print(exec(program)[1])
+print(next(r[1] for r in (exec([*program[:idx], ["jmp" if i == "nop" else "nop", v], *program[idx + 1:]]) for idx, (i, v) in enumerate(program) if i != "acc") if r[0]))
