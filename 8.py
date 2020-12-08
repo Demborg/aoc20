bda507f101b5aa15b2af5b15ce28070e777feab1
Axel Demborg <axel.demborg@volumental.com>
2020-12-08 20:56:01 +0100

    8.2 solved

--- 8.py
+++ 8.py
@@ -3,13 +3,14 @@ from sys import stdin
 
 program = [[i, int(v)] for i, v in re.findall("(\w{3}) (.\d*)", stdin.read())]
 
-def exec(program, idx, acc, visited):
-    if idx >= len(program) or (err := idx in visited):
-        return (err, acc) 
+def exec(program, visited, idx=0, acc=0):
+    if (end := idx >= len(program)) or idx in visited:
+        return (end, acc) 
     visited.add(idx)
     i, v = program[idx]
     idx += v if i == "jmp" else 1
     acc += v if i == "acc" else 0
-    return exec(program, idx, acc, visited)
+    return exec(program, visited, idx, acc)
         
-print(exec(program, 0, 0, set())[1])
+print(exec(program, set())[1])
+print(next(r for r in (exec([*program[:idx], ["jmp" if i == "nop" else "nop", v], *program[idx + 1:]], set()) for idx, (i, v) in enumerate(program) if i != "acc") if r[0]))
