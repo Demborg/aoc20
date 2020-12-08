5bbe178281bf22acc1aed0e7acf3219cad0f875d
Axel Demborg <axel.demborg@volumental.com>
2020-12-08 20:21:36 +0100

    recursion

--- 8.py
+++ 8.py
@@ -3,13 +3,13 @@ from sys import stdin
 
 program = [[i, int(v)] for i, v in re.findall("(\w{3}) (.\d*)", stdin.read())]
 
-idx = 0
-visited = set()
-acc = 0
-while idx not in visited:
+def exec(program, idx, acc, visited):
+    if idx >= len(program) or (err := idx in visited):
+        return (err, acc) 
     visited.add(idx)
     i, v = program[idx]
     idx += v if i == "jmp" else 1
     acc += v if i == "acc" else 0
+    return exec(program, idx, acc, visited)
         
-print(acc)
+print(exec(program, 0, 0, set())[1])
