fc4b0c1cf3fa5ffed3e30b2e7f972b8262698676
Axel Demborg <axel.demborg@volumental.com>
2020-12-09 08:34:45 +0100

    not pretty but part 2 solved

--- 9.py
+++ 9.py
@@ -3,4 +3,16 @@ from sys import stdin
 PRE = 25
 
 data = [int(i) for i in stdin.read().split("\n") if i]
-print(next(v for i, v in enumerate(data[PRE:]) if v not in {sum(c) for c in combinations(data[i: PRE + i], 2)}))
+inv = next(v for i, v in enumerate(data[PRE:]) if v not in {sum(c) for c in combinations(data[i: PRE + i], 2)})
+print(inv)
+
+for i in range(len(data)):
+    l = []
+    d = 0
+    while sum([0, *l]) < inv:
+        l.append(data[i+d])
+        d += 1    
+    if sum(l) == inv and d > 1:
+        print(min(l) + max(l))
+        
+
