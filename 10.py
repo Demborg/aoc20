9a42ce64446c79b868dea967ff7ca61d43c38975
Axel Demborg <axel.demborg@volumental.com>
2020-12-11 22:14:29 +0100

    faster but not nearly good enough

--- 10.py
+++ 10.py
@@ -6,8 +6,7 @@ diffs = lambda l: (b - a for a, b in zip([0, *l], [*l, data[-1] + 3]))
 print(prod(sum(d == i for d in diffs(data)) for i in (1, 3)))
 
 def f(l, floor=0):
-    if any(d > 3 for d in diffs(l)):
-        return 0
-    return 1 + sum(f(l[:i] + l[i + 1:], v) for i, v in enumerate(l) if v > floor) 
+    ds = list(diffs(l))
+    return 1 + sum(f(l[:i] + l[i + 1:], v) for i, v in enumerate(l) if l[i] > floor and ds[i] + ds[i +1] < 4) 
 
 print(f(data))
