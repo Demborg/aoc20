25921d10f3938546dd1289a4b7c00761ddf337b7
Axel Demborg <axel.demborg@volumental.com>
2020-12-11 22:56:27 +0100

    recursive solution attempt, still to slow

--- 10.py
+++ 10.py
@@ -1,12 +1,13 @@
 from math import prod
 from sys import stdin
+from itertools import groupby
 
 data = sorted([int(l) for l in stdin.read().split("\n") if l])
-diffs = lambda l: (b - a for a, b in zip([0, *l], [*l, data[-1] + 3]))
-print(prod(sum(d == i for d in diffs(data)) for i in (1, 3)))
+data = [0, *data, max(data) + 3]
+print(prod(sum(b - a == i for a, b in zip(data, data[1:])) for i in (1, 3)))
 
-def f(l, floor=0):
-    ds = list(diffs(l))
-    return 1 + sum(f(l[:i] + l[i + 1:], v) for i, v in enumerate(l) if l[i] > floor and ds[i] + ds[i +1] < 4) 
+tree = {l: [x[1] for x in g] for l, g in groupby([(i, j) for i in data for j in data if j > i and j - i < 4], lambda x: x[0])}
+def f(v):
+    return 1 if v == max(data) else sum(f(n) for n in tree[v])
 
-print(f(data))
+print(f(0))
