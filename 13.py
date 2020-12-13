08c4b1d222884257bc7a719b70be8c9e2a1b324f
Axel Demborg <axel.demborg@volumental.com>
2020-12-13 18:32:51 +0100

    fun recursive iterator, even slower thuogh...

--- 13.py
+++ 13.py
@@ -4,5 +4,10 @@ from sys import stdin
 t = int(stdin.readline())
 bs = [None if b == "x" else int(b) for b in stdin.readline().split(",")]
 print(min(((w := b - t % b), b * w) for b in bs if b))
-max_b, max_i = max((b, i) for i, b in enumerate(bs) if b)
-print(next(t for t in count(max_b - max_i, max_b) if all((t + i) % b == 0 for i, b in enumerate(bs) if b)))
+
+def iter(i=0):
+    if i >= len(bs):
+        yield from count()
+    yield from (t for t in iter(i + 1) if bs[i] is None or (t + i) % bs[i] == 0) 
+
+print(next(iter()))
