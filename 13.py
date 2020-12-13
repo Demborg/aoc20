ec491e1b8d4afd5d3e83ca5a73450a91c4170985
Axel Demborg <axel.demborg@volumental.com>
2020-12-13 12:33:33 +0100

    simplification before 13.2

--- 13.py
+++ 13.py
@@ -1,8 +1,5 @@
-from math import ceil
 from sys import stdin
 
 t = int(stdin.readline())
-bs = [int(b) for b in stdin.readline().split(",") if b != "x"]
-print(t, bs)
-
-print(min(((w := b * ceil(t / b) - t), b * w) for b in bs))
+bs = [None if b == "x" else int(b) for b in stdin.readline().split(",")]
+print(min(((w := b - t % b), b * w) for b in bs if b))
