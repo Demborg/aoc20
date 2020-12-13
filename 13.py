623ee0a6f8509a7afeb38670e22c23e0f9860175
Axel Demborg <axel.demborg@volumental.com>
2020-12-13 13:02:50 +0100

    part 13.2 seems to be working on test cases too slow though

--- 13.py
+++ 13.py
@@ -1,5 +1,8 @@
+from itertools import count
 from sys import stdin
 
 t = int(stdin.readline())
 bs = [None if b == "x" else int(b) for b in stdin.readline().split(",")]
 print(min(((w := b - t % b), b * w) for b in bs if b))
+max_b, max_i = max((b, i) for i, b in enumerate(bs) if b)
+print(next(t for t in count(max_b - max_i, max_b) if all((t + i) % b == 0 for i, b in enumerate(bs) if b)))
