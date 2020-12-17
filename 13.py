5808909596f58d4e740af5ac1af2b520dca93c7b
Axel Demborg <axel.demborg@volumental.com>
2020-12-17 22:24:36 +0100

    learning a bit about chineese rest theorem to solve 13.2

--- 13.py
+++ 13.py
@@ -1,14 +1,20 @@
 from itertools import count
 from sys import stdin
+from math import prod
 
 t = int(stdin.readline())
-bs = sorted([(int(b), i) for i, b in enumerate(stdin.readline().split(",")) if b != "x"])
+bs = [(int(b), i) for i, b in enumerate(stdin.readline().split(",")) if b != "x"]
 print(min(((w := b - t % b), b * w) for b, _ in bs))
 
-def iter(i=0):
-    b, j = bs[i]
-    if i == len(bs) - 1:
-        yield from count(b - j, b)
-    yield from (t for t in iter(i + 1) if (t + j) % b == 0) 
-
-print(next(iter()))
+print(
+        sum(
+            next(
+                n for j in range(b1) 
+                if (n := (j * prod(
+                    b2 for b2, _ in bs if b2 != b1
+                ))) % b1 == -i % b1
+            )
+            for b1, i in bs
+        ) 
+        % prod(b for b, i in bs)
+)
